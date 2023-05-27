d5a2a68a391b09c464c0e30bed974c3a1206fe45
Bright Adams <brightadams58@gmail.com>
2023-05-27 06:28:05 -0700

    updated

--- printf.c
+++ printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - the entry point of the app
  *
  *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("checking the printf function,\n");
 	return (0);
 }
 
